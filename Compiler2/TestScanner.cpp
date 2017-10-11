9d163740f6e1b2840e554135a2d46d0242fe4c80
Jack Pettit <skelatorjack@gmail.com>
2017-10-11 16:39:22 -0500

    Created TestScanner object.

---
+++ Compiler2/TestScanner.cpp
@@ -0,0 +1,9 @@
+//
+//  TestScanner.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 10/11/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "TestScanner.hpp"
---
+++ Compiler2/TestScanner.hpp
@@ -0,0 +1,14 @@
+//
+//  TestScanner.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 10/11/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef TestScanner_hpp
+#define TestScanner_hpp
+
+#include <stdio.h>
+
+#endif /* TestScanner_hpp */
