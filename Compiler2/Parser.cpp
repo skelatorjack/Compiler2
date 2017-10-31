5601a55caef221e125f93394692f65512f04e006
Jack Pettit <skelatorjack@gmail.com>
2017-10-31 13:47:44 -0500

    Created function declarations for all of Parser's Methods.

--- Compiler2/Parser.cpp
+++ Compiler2/Parser.cpp
@@ -7,3 +7,109 @@
 //
 
 #include "Parser.hpp"
+
+Parser::Parser(string file_name, bool has_seen_comment, bool was_file_redirected) : m_scanner(file_name, has_seen_comment, was_file_redirected), m_parseTree(nullptr) {
+    
+}
+
+Parser::~Parser() {
+    deinit();
+}
+
+void Parser::runParser() {
+    
+}
+
+void Parser::setCurrentToken(const Token NEW_TOKEN) {
+    m_currentToken = NEW_TOKEN;
+}
+
+Token Parser::getCurrentToken() const {
+    return m_currentToken;
+}
+
+ParseTree Parser::getParseTree() const {
+    return m_parseTree;
+}
+
+void Parser::setParseTree(const ParseTree NEW_PARSE_TREE) {
+    m_parseTree = NEW_PARSE_TREE;
+}
+
+// private methods
+
+void Parser::getTokenFromScanner() {
+    setCurrentToken(m_scanner.buildToken());
+}
+
+void Parser::deinit() {
+
+}
+
+void Parser::program() {
+    
+}
+
+void Parser::block() {
+    
+}
+
+void Parser::vars() {
+    
+}
+
+void Parser::mvars() {
+    
+}
+
+void Parser::expr() {
+    
+}
+
+void Parser::M() {
+    
+}
+
+void Parser::F() {
+    
+}
+
+void Parser::T() {
+    
+}
+
+void Parser::R() {
+    
+}
+
+void Parser::stats() {
+    
+}
+
+void Parser::mStat() {
+    
+}
+
+void Parser::in() {
+    
+}
+
+void Parser::out() {
+    
+}
+
+void Parser::If() {
+    
+}
+
+void Parser::loop() {
+    
+}
+
+void Parser::assign() {
+    
+}
+
+void Parser::RO() {
+    
+}
