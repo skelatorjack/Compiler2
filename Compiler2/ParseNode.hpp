01e8b2d665b1929c39d4edcacd2f429cd3ea7a2d
Jack Pettit <skelatorjack@gmail.com>
2017-10-31 13:01:32 -0500

    Created ParseNode class

--- Compiler2/ParseNode.hpp
+++ Compiler2/ParseNode.hpp
@@ -9,6 +9,36 @@
 #ifndef ParseNode_hpp
 #define ParseNode_hpp
 
-#include <stdio.h>
+#include "Includes.hpp"
+#include "Token.hpp"
+
+enum subtree {
+   firstChild, secondChild, thirdChild, fourthChild
+};
+
+class ParseNode {
+private:
+    Token m_storedToken;
+    shared_ptr<ParseNode> m_firstChild;
+    shared_ptr<ParseNode> m_secondChild;
+    shared_ptr<ParseNode> m_thirdChild;
+    shared_ptr<ParseNode> m_fourthChild;
+    string m_nonTerminal;                   // Mark which nonterminal this node was created in
+    
+    void deinit();
+    
+public:
+    ParseNode(shared_ptr<ParseNode> = nullptr, string = "");
+    ~ParseNode();
+    void setStoredToken(const Token);
+    Token getStoredToken() const;
+    
+    void setChild(shared_ptr<ParseNode>, subtree);
+    shared_ptr<ParseNode> getChild() const;
+    
+    void setNonTerminal(string = "");
+    string getNonTerminal() const;
+    
+};
 
 #endif /* ParseNode_hpp */
