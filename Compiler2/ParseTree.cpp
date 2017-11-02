af73a20ed44e27575e2379a0c5cab52d68ea963a
Jack Pettit <skelatorjack@gmail.com>
2017-11-01 22:26:11 -0500

    Implemented some of ParseTree's methods.

--- Compiler2/ParseTree.cpp
+++ Compiler2/ParseTree.cpp
@@ -7,3 +7,25 @@
 //
 
 #include "ParseTree.hpp"
+
+ParseTree::ParseTree(shared_ptr<ParseNode> new_root) : m_root(new_root) {
+    
+}
+
+ParseTree::~ParseTree() {
+    
+}
+
+void ParseTree::setRoot(shared_ptr<ParseNode> newRoot) {
+    m_root = newRoot;
+}
+
+shared_ptr<ParseNode> ParseTree::getRoot() const {
+    return m_root;
+}
+
+void ParseTree::traverseTree(shared_ptr<ParseNode> root) {
+    
+}
+
+
