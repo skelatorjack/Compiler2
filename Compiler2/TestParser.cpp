f89ca5bec5a9703fc0a5bb69b45f5a5093ea5045
Jack Pettit <skelatorjack@gmail.com>
2017-11-03 23:06:37 -0500

    Created TestParser.

---
+++ Compiler2/TestParser.cpp
@@ -0,0 +1,9 @@
+//
+//  TestParser.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/3/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "TestParser.hpp"
---
+++ Compiler2/TestParser.hpp
@@ -0,0 +1,14 @@
+//
+//  TestParser.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/3/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef TestParser_hpp
+#define TestParser_hpp
+
+#include <stdio.h>
+
+#endif /* TestParser_hpp */
