585ac82d342094f42711c7779233e2503d861b8e
Jack Pettit <skelatorjack@gmail.com>
2017-11-15 19:12:21 -0600

    Created StaticSem header and source.

---
+++ Compiler2/StaticSem.cpp
@@ -0,0 +1,9 @@
+//
+//  StaticSem.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/15/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "StaticSem.hpp"
---
+++ Compiler2/StaticSem.hpp
@@ -0,0 +1,14 @@
+//
+//  StaticSem.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/15/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef StaticSem_hpp
+#define StaticSem_hpp
+
+#include <stdio.h>
+
+#endif /* StaticSem_hpp */
