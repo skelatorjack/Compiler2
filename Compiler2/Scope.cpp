044ac4a575258ce328cfdca29a408fb2d8d34040
Jack Pettit <skelatorjack@gmail.com>
2017-11-15 19:12:03 -0600

    Created Scope header and source files.

---
+++ Compiler2/Scope.cpp
@@ -0,0 +1,9 @@
+//
+//  Scope.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/15/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "Scope.hpp"
---
+++ Compiler2/Scope.hpp
@@ -0,0 +1,14 @@
+//
+//  Scope.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/15/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef Scope_hpp
+#define Scope_hpp
+
+#include <stdio.h>
+
+#endif /* Scope_hpp */
