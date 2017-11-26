00cf09e61b82dcf4b8416fcb1bbe770ddde35e8a
Jack Pettit <skelatorjack@gmail.com>
2017-11-26 16:11:38 -0600

    Added source and header for CodeGenerator.

---
+++ Compiler2/CodeGenerator.cpp
@@ -0,0 +1,9 @@
+//
+//  CodeGenerator.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/26/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "CodeGenerator.hpp"
---
+++ Compiler2/CodeGenerator.hpp
@@ -0,0 +1,14 @@
+//
+//  CodeGenerator.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 11/26/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef CodeGenerator_hpp
+#define CodeGenerator_hpp
+
+#include <stdio.h>
+
+#endif /* CodeGenerator_hpp */
