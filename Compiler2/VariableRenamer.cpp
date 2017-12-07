b364dfa7e02d8793c37f0cecbd37a6bf0bc33221
Jack Pettit <skelatorjack@gmail.com>
2017-12-07 11:31:05 -0600

    Added source files for VariableRenamer.

---
+++ Compiler2/VariableRenamer.cpp
@@ -0,0 +1,9 @@
+//
+//  VariableRenamer.cpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 12/7/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#include "VariableRenamer.hpp"
---
+++ Compiler2/VariableRenamer.hpp
@@ -0,0 +1,14 @@
+//
+//  VariableRenamer.hpp
+//  Compiler2
+//
+//  Created by Jack Pettit on 12/7/17.
+//  Copyright © 2017 Jack Pettit. All rights reserved.
+//
+
+#ifndef VariableRenamer_hpp
+#define VariableRenamer_hpp
+
+#include <stdio.h>
+
+#endif /* VariableRenamer_hpp */
